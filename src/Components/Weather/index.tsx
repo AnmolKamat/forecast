00630fc20687db1a0df1e7d72f059a653803301d
AnmolKamat <anmolrk096322@gmail.com>
2023-12-07 10:35:45 +0530

    Loading animation and update in meta data

--- src/Components/Weather/index.tsx
+++ src/Components/Weather/index.tsx
@@ -2,6 +2,7 @@ import { WeatherData, cityInterface } from "@/Types";
 import { IconKey, icons } from "@/utils/Icons";
 import { queries } from "@/utils/graphql";
 import { useQuery } from "@apollo/client";
+import { Spinner } from "@nextui-org/react";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -10,12 +11,13 @@ type Props = {
 };
 
 const WeatherContanier = ({ city }: Props) => {
-  const { refetch: getWeather, data: weatherData } = useQuery(
-    queries.GET_WEATHER,
-    {
-      variables: { lat: city.lat, lon: city.lon },
-    }
-  );
+  const {
+    refetch: getWeather,
+    data: weatherData,
+    loading,
+  } = useQuery(queries.GET_WEATHER, {
+    variables: { lat: city.lat, lon: city.lon },
+  });
   const [weather, setWeather] = useState<WeatherData>(weatherData);
   useEffect(() => {
     const fetchWeather = async () => {
@@ -66,47 +68,65 @@ const WeatherContanier = ({ city }: Props) => {
   }, [weather]);
   return (
     <div className="flex mt-24 mx-4 justify-between -z-20">
-      <div className="bg-white/30 backdrop-blur-xl text-black w-fit rounded-xl p-8 min-w-[30rem]">
-        {weather && (
+      <div className="bg-white/30 backdrop-blur-xl text-black w-fit rounded-xl p-8 min-w-[30rem] shadow-2xl ">
+        {loading ? (
+          <div className="h-44 flex justify-center items-center">
+            <Spinner size="lg" color="default" />
+          </div>
+        ) : (
           <>
-            <div className="flex items-center">
-              <Image
-                src={icons[`icon_${weather.weather[0].icon}` as IconKey].src}
-                alt="icons"
-                width={200}
-                height={200}
-                className=" drop-shadow-2xl"
-              />
-              <div className="flex flex-col gap-4">
-                <h1 className="font-bold text-xl capitalize">
-                  {weather.weather[0].description}
-                </h1>
-                <h1 className="text-5xl">
-                  {Math.floor(weather.main.temp - 273.15)}
-                  <span className="text-xl">°C</span>
-                </h1>
-                <p>
-                  Feels like {Math.floor(weather.main.feels_like - 273.15)}
-                  <span>°C</span>
-                </p>
-              </div>
-            </div>
+            {weather && (
+              <>
+                <div className="flex items-center">
+                  <Image
+                    src={
+                      icons[`icon_${weather.weather[0].icon}` as IconKey].src
+                    }
+                    alt="icons"
+                    width={200}
+                    height={200}
+                    className=" drop-shadow-2xl"
+                  />
+                  <div className="flex flex-col gap-4">
+                    <h1 className="font-bold text-xl capitalize">
+                      {weather.weather[0].description}
+                    </h1>
+                    <h1 className="text-5xl">
+                      {Math.floor(weather.main.temp - 273.15)}
+                      <span className="text-xl">°C</span>
+                    </h1>
+                    <p>
+                      Feels like {Math.floor(weather.main.feels_like - 273.15)}
+                      <span>°C</span>
+                    </p>
+                  </div>
+                </div>
+              </>
+            )}
           </>
         )}
       </div>
-      <div className="bg-white/30 backdrop-blur-xl text-black w-fit rounded-xl p-8 min-w-[30rem] text-end ">
-        <h1 className=" font-bold text-3xl">{city.name}</h1>
-        <h1 className="text-xl">
-          {city.state && `${city.state},`} {countryName.of(city.country)}
-        </h1>
-        <h1 className={`font-light text-[4rem] `}>
-          {currentTime.h}
-          <span className=" w-14">{isBlinking ? ":" : <>&nbsp;</>}</span>
-          {currentTime.m} {currentTime.ampm}
-        </h1>
-        <h1 className="text-xl">
-          {getTimeAndDate(weather && +weather.timezone).date}
-        </h1>
+      <div className="bg-white/30 backdrop-blur-xl text-black w-fit rounded-xl p-8 min-w-[30rem] text-end shadow-2xl ">
+        {loading ? (
+          <div className="h-44 flex justify-center items-center">
+            <Spinner size="lg" color="default" />
+          </div>
+        ) : (
+          <>
+            <h1 className=" font-bold text-3xl">{city.name}</h1>
+            <h1 className="text-xl">
+              {city.state && `${city.state},`} {countryName.of(city.country)}
+            </h1>
+            <h1 className={`font-light text-[4rem] `}>
+              {currentTime.h}
+              <span className=" w-14">{isBlinking ? ":" : <>&nbsp;</>}</span>
+              {currentTime.m} {currentTime.ampm}
+            </h1>
+            <h1 className="text-xl">
+              {getTimeAndDate(weather && +weather.timezone).date}
+            </h1>
+          </>
+        )}
       </div>
     </div>
   );
